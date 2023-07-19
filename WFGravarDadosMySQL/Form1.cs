cfe2597f89491ff338e0dd2b5a016ca03139464f
ANA LAURA CLEMENTINO ALMEIDA <ana.lcalmeida1@senacsp.edu.br>
2023-07-19 15:20:06 -0300

    abc

---
+++ WFGravarDadosMySQL/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WFGravarDadosMySQL
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection Conexao;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=;database=db_agenda";
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "INSERT INTO contato (noe,email,telefone)" +
+                    "VALUES" +
+                    "(' " + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+                Conexao.Open();
+                comando.ExecuteReader();
+
+                MessageBox.Show("Cadastro inserido cpm sucesso!");
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
